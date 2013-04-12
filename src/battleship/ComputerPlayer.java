db14ea7c4627171656e81564b417ba8391c9dcc0
Wiktor Kettel <wiktor@kettel.se>
2013-04-12 14:08:35 +0200

    La till en tom ComputerPlayer

---
+++ src/battleship/ComputerPlayer.java
@@ -0,0 +1,28 @@
+
+package battleship;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class ComputerPlayer extends Player {
+    /**
+     * TODO: Implement a functional computer player
+     *
+     */
+	private static int VERTICAL = 0;
+	private static int HORIZONTAL = 1;
+
+	public ComputerPlayer() {
+
+	}
+
+	@Override
+	public void makeMove(Coordinate c) {
+
+	}
+
+	@Override
+	public void placeShips(Map map) {
+	}
+
+}
